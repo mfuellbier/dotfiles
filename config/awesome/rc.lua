c15d06a55925517742bc21050ee1a907100f9e43
Michael Fuellbier <michael.fuellbier@syss.de>
2017-01-20 11:43:36 +0100

    update

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -476,7 +476,10 @@ end
 
 clientbuttons = awful.util.table.join(
     awful.button({ }, 1, function (c) client.focus = c; c:raise() end),
+    awful.button({ modkey }, 1, function (c) client.focus = c; c:raise() end),
     awful.button({ modkey }, 1, awful.mouse.client.move),
+    awful.button({ "Shift" }, 1, function (c) client.focus = c; c:raise() end),
+    awful.button({ "Shift" }, 1, awful.mouse.client.move),
     awful.button({ modkey }, 3, awful.mouse.client.resize))
 
 -- Set keys
