b1b27ed80c1e24e24a7a2b2a58e4a44ef7b1493d
Michael Füllbier <michael.fuellbier@posteo.de>
2020-11-09 18:43:30 +0100

    Remove opacity on unfocused windows

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -611,7 +611,7 @@ client.connect_signal("mouse::enter", function(c)
 end)
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
-client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal c.opacity = 1 end)
 -- }}}
 
 -- battery warning
